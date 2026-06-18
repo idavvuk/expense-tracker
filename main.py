396d4f9a760c2711ac2c1c0d18bf0c3be5f3a29e
Ida Vranvuk <ida.vranvuk@hotmail.com>
2026-06-18 23:02:16 +0200

    small fix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import vault
 
 def start(): 
-    choice = input("Add expense [A], View expenses [V]").lower() 
+    choice = input("Add expense [A], View expenses [V]: ").lower() 
     if choice == "a":
         vault.add_expense()
     if choice == "v":
