a7c59bc4d309c105db741e9506d154bf0a2ca7d1
Ida Vranvuk <ida.vranvuk@hotmail.com>
2026-06-23 21:52:28 +0200

    new main() function

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import vault
+import sys
+import pandas as pd # type: ignore
 
-def start(): 
-    choice = input("Add expense [A], View expenses [V]: ").lower() 
-    if choice == "a":
-        vault.add_expense()
-    if choice == "v":
-        vault.view_expenses()
+
+def main(): 
+    while True: 
+        print("Welcome to your expense tracker!")
+        while True: 
+            choice = input("Choose one: [A] Add expense, [V] View expense").lower()
+        if choice == "a":
+            vault.add_expense()
+        elif choice == "v":
+            vault.view_expenses
+        else: 
+            print("Invalid choice")
+main()
+        
+            
+        
--- vault.py
+++ vault.py
@@ -1,24 +1,34 @@
 import csv
 
+expenses_file_path = 'expenses.csv'
 
 
 
-entry = []
-all_entries = [[]]
 
 def add_expense(): 
-    while True: 
-        date = input("add date ")
-        amount = input("add amount ")
-        category = input("add category ")
-        entry.append({date, amount, category })
-        if date == "":
-            break
-        all_entries.append({entry})
-    print(all_entries)
+    date = input("add date ")
+    amount = input("add amount ")
+    category = input("add category ")
+
+
+    with open(expenses_file_path, 'w', newline = '') as csvfile:
+        w = csv.writer(csvfile)
+        w.writerow([date, amount, category])
+        print("Expense successfully added!")
 
 def view_expenses():
-    print(all_entries)
+    
+    with open("expenses_file_path", "r") as file: 
+        reader = csv.reader(file)
+        next(reader) #what does reader do? 
+        for row in reader: 
+            print(row)
+
+
+
+
+
+
 
 
 
